c263d2cb385d923d0a86e7cac2349b28b756d4d9
Adrián Antón <adrtler@gmail.com>
2022-03-02 11:48:30 +0100

    Creando un Flux (Observable) a partir de un List o Iterable

--- src/main/java/com/bolsaideas/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/bolsaideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,8 @@
 package com.bolsaideas.springboot.reactor.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -23,9 +26,9 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 //		Flux<String> nombres = Flux.just("Adrián", "Ana", "Pedro").doOnNext(elemento -> System.out.println(elemento));
 //		nombres.subscribe();
-		Flux<String> nombres = Flux.just("Adrián Antón", "Pepe Pep", "Pedro fulano", "Ana Romeu", "María Mar",
+		Flux<String> names = Flux.just("Adrián Antón", "Pepe Pep", "Pedro fulano", "Ana Romeu", "María Mar",
 				"Bruce Lee", "Bruce Willis");
-		Flux<Usuario> usuarios = nombres
+		Flux<Usuario> users = names
 				.map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
 				.filter(usuario -> usuario.getNombre().equalsIgnoreCase("bruce")).doOnNext(usuario -> {
 					if (usuario == null) {
@@ -39,12 +42,24 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 					return u;
 				});
 //		nombres.subscribe(log::info);
-		usuarios.subscribe(e -> log.info(e.toString()), error -> log.error(error.getMessage()), new Runnable() {
+		users.subscribe(e -> log.info(e.toString()), error -> log.error(error.getMessage()), new Runnable() {
 			@Override
 			public void run() {
 				log.info("Ha finalizado la ejecucción del observable con éxito!");
 			}
 		});
+
+		List<String> usuarios = new ArrayList<String>();
+		usuarios.add("Adrián Antón");
+		usuarios.add("Pepe Pep");
+		usuarios.add("Pedro fulano");
+		usuarios.add("Ana Romeu");
+		usuarios.add("María Mar");
+		usuarios.add("Bruce Lee");
+		usuarios.add("Bruce Willis");
+
+		Flux<String> nombres = Flux.fromIterable(usuarios);
+
 	}
 
 }
